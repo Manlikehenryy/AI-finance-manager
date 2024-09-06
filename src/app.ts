cbcddf8c7a0ebba8313450e80814a465f16943d8
manlikehenryy <mbamaluhenry8@gmail.com>
2024-09-06 16:43:35 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import authRoutes from "./routes/auth.route";
 import transactionRoutes from "./routes/transaction.route";
 import { errorHandler } from "./utils/errorHandler";
 import protectRoute from "./middleware/protectRoute";
-import { Resend } from "resend";
 
 dotenv.config();
 
@@ -15,23 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-const resend = new Resend("re_4jcNAWa9_7o3HbCFGgBGu6YUKHJHXd6M7");
 
 const port = process.env.PORT || 3000;
-app.get("/email", async (req: Request, res: Response) => {
-  const { data, error } = await resend.emails.send({
-    from: "noreply@resend.dev",
-    to: "henrymbamalu1@gmail.com",
-    subject: "Hello World",
-    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-  });
-
-  if (error) {
-    return res.status(400).json({ error });
-  }
-
-  res.status(200).json({ data });
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/transaction", protectRoute, transactionRoutes);
